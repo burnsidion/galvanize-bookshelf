3dba873fac4a92a07d36df35cac71369cd89cd35
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-04-02 20:37:20 -0600

    added migration and initial set ups

---
+++ migrations/20180402180951_books.js
@@ -0,0 +1,35 @@
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('books', (table) => {
+    table.increments()
+    table.varchar('title', 255).notNullable().defaultTo('')
+    table.varchar('author', 255).notNullable().defaultTo('')
+    table.varchar('genre', 255).notNullable().defaultTo('')
+    table.text('description').notNullable().defaultTo('')
+    table.text('cover_url').notNullable().defaultTo('')
+    table.dateTime('created_at').notNullable()
+    table.dateTime('updated_at').notNullable()
+  })
+};
+
+
+
+
+
+exports.down = (knex, Promise) => knex.schema.dropTableIfExists('books')
+
+/*
+```text
+┌──────────────────────────────────────────────────────────────────────────────────────────┐
+│                                          books                                           │
+├─────────────┬─────────────────────────┬──────────────────────────────────────────────────┤
+│id           │serial                   │primary key                                       │
+│title        │varchar(255)             │not null default ''                               │
+│author       │varchar(255)             │not null default ''                               │
+│genre        │varchar(255)             │not null default ''                               │
+│description  │text                     │not null default ''                               │
+│cover_url    │text                     │not null default ''                               │
+│created_at   │timestamp with time zone │not null default now()                            │
+│updated_at   │timestamp with time zone │not null default now()                            │
+└─────────────┴─────────────────────────┴──────────────────────────────────────────────────┘
+```
+*/
