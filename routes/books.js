24ab1358382dd32307563cbd31703a9e9baaa3b8
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-04-03 20:47:12 -0600

    3 tests passing for part 2

--- routes/books.js
+++ routes/books.js
@@ -4,7 +4,65 @@ const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
-
+const knex = require('../knex');
+const humps = require('humps')
 // YOUR CODE HERE
+
+router.get('/books', (req, res, next) => {
+  knex('books')
+    .then((data) => {
+      data.sort((a, b) => {
+        if (a.title > b.title) {
+          return 1
+        } else {
+          return -1
+        }
+      })
+      res.json(humps.camelizeKeys(data))
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
+
+router.get('/books/:id', (req, res, next) => {
+  knex('books')
+    .where('id', req.params.id)
+    .then((data) => {
+      res.json(humps.camelizeKeys(data[0]))
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
+
+router.post('/books', (req, res, next) => {
+  knex('books')
+  .insert(humps.decamelizeKeys({
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre,
+    description: req.body.description,
+    cover_url: req.body.coverUrl
+  }))
+  .returning('*')
+  .then((data) => {
+
+    res.json(humps.camelizeKeys(data[0]))
+  })
+  .catch((err) => {
+    next(err)
+  })
+})
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
