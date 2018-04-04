754739ecbf2b1da4fb56692233a685f99454bc50
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-04-04 14:33:38 -0600

    part 2 tests passing

--- routes/books.js
+++ routes/books.js
@@ -38,31 +38,67 @@ router.get('/books/:id', (req, res, next) => {
 
 router.post('/books', (req, res, next) => {
   knex('books')
-  .insert(humps.decamelizeKeys({
-    title: req.body.title,
-    author: req.body.author,
-    genre: req.body.genre,
-    description: req.body.description,
-    cover_url: req.body.coverUrl
-  }))
-  .returning('*')
-  .then((data) => {
+    .insert(humps.decamelizeKeys({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      description: req.body.description,
+      cover_url: req.body.coverUrl
+    }))
+    .returning('*')
+    .then((data) => {
 
-    res.json(humps.camelizeKeys(data[0]))
-  })
-  .catch((err) => {
-    next(err)
-  })
+      res.json(humps.camelizeKeys(data[0]))
+    })
+    .catch((err) => {
+      next(err)
+    })
 })
 
+router.patch('/books/:id', (req, res, next) => {
+  knex('books')
+    .where('id', req.params.id)
+    .then((data) => {
+      knex('books')
+        .where('id', req.params.id)
+        .limit(1)
+        .update(humps.decamelizeKeys({
+          title: req.body.title,
+          author: req.body.author,
+          genre: req.body.genre,
+          description: req.body.description,
+          cover_url: req.body.coverUrl
+        }))
+        .returning('*')
+        .then((data) => {
+          res.json(humps.camelizeKeys(data[0]))
+        })
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
 
-
-
-
-
-
-
-
-
+router.delete('/books/:id', (req, res, next) => {
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((data) => {
+      if (!data) return next()
+      const decammed = humps.decamelizeKeys(data)
+      const newData = {
+        title: decammed.title,
+        author: decammed.author,
+        genre: decammed.genre,
+        description: decammed.description,
+        cover_url: decammed.cover_url
+      }
+      return knex('books')
+        .where('id', req.params.id)
+        .then((row) => {
+          res.json(humps.camelizeKeys(newData))
+        })
+    })
+})
 
 module.exports = router;
