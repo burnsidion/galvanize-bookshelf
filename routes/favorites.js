eced4912d063cf2df4ecea86f9ce4e78b88cdd07
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-04-09 10:46:30 -0600

    heroku deployment

--- routes/favorites.js
+++ routes/favorites.js
@@ -14,59 +14,75 @@ const bcrypt = require('bcrypt');
 // YOUR CODE HERE
 
 router.get('/favorites', (req, res, next) => {
-  knex.select('*').from('favorites')
-    .leftJoin('books', 'books.id', 'favorites.book_id')
-    .then((data) => {
-      res.send(humps.camelizeKeys(data))
-    })
-    .catch((err) => {
-      next(err)
-    })
+  if(req.cookies.token){
+    knex.select('*').from('favorites')
+      .join('books', 'books.id', 'favorites.book_id')
+      .then((data) => {
+        res.send(humps.camelizeKeys(data))
+      })
+  } else {
+    res.status(401).type('text/plain').send('Unauthorized')
+  }
+
+
 })
 
 router.get('/favorites/check?', (req, res, next) => {
-  knex('favorites')
-    .select('*')
-    .where('book_id', req.query.bookId)
-    .then(data => {
-      if (data.length > 0) {
-        res.json(true)
-      } else {
-        res.json(false)
-      }
-    })
+  if (req.cookies.token) {
+    knex('favorites')
+      .select('*')
+      .where('book_id', req.query.bookId)
+      .then(data => {
+        if (data.length > 0) {
+          res.json(true)
+        } else {
+          res.json(false)
+        }
+      })
+  } else {
+    res.status(401).type('text/plain').send('Unauthorized')
+  }
+
 })
 
 router.post('/favorites', (req, res, next) => {
-  knex('favorites')
-  .select('*')
-    .insert({
-      id: req.body.id,
-      book_id: req.body.bookId,
-      user_id: 1
-    })
-    .returning('*')
-    .then(data => {
-      res.json(humps.camelizeKeys(data[0]))
-    })
-    .catch((err) => {
-      next(err)
-    })
+  if (req.cookies.token) {
+    let token = jwt.decode(req.cookies.token)
+    console.log(token.id);
+    knex('favorites')
+      .select('*')
+      .insert({
+        id: req.body.id,
+        book_id: req.body.bookId,
+        user_id: token.id
+      })
+      .returning('*')
+      .then(data => {
+        res.json(humps.camelizeKeys(data[0]))
+      })
+
+  } else {
+    res.status(401).type('text/plain').send('Unauthorized')
+  }
+
 
 })
 
 router.delete('/favorites', (req, res, next) => {
-  knex('favorites')
-  .where('book_id', req.body.bookId)
-  .del()
-  .returning(['book_id', 'user_id'])
-  .then(data => {
-    res.json(humps.camelizeKeys(data[0]))
+  if (req.cookies.token) {
+    knex('favorites')
+      .where('book_id', req.body.bookId)
+      .del()
+      .returning(['book_id', 'user_id'])
+      .then(data => {
+        res.json(humps.camelizeKeys(data[0]))
 
-  })
-  .catch((err) => {
-    next(err)
-  })
+      })
+
+  } else {
+    res.status(401).type('text/plain').send('Unauthorized')
+  }
+
 })
 
 
